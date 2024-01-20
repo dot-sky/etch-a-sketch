ab7924e5cd6d903d00e4496c16cb3033ed14e8da
dot-sky <108539845+dot-sky@users.noreply.github.com>
2024-01-20 08:05:19 -0400

    Add feature to change number of boxes per side

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-const container = document.querySelector(".container");
-for (let i = 0; i < 16*16; i++) {
-    const element = document.createElement("div");
-    element.className = "square default";
-    container.appendChild(element);
-    element.addEventListener("mouseover",() => {
-        element.classList.remove("default");
-        element.classList.add("box-hover");
-    });
-    element.addEventListener("mouseout",() => {
-        element.classList.remove("box-hover");
-        element.classList.add("default");
-    });
+function createGird(squaresPerSide){
+    container.remove();
+    container = document.createElement("div");
+    container.className = "container";
+    const boxSize = Math.floor(GRID_HEIGHT/squaresPerSide);
+    console.log(boxSize);
+    for(let i=0; i<squaresPerSide; i++){
+        const currentRow = document.createElement("div");
+        currentRow.className = "row";
+        for (let i = 0; i < squaresPerSide; i++) {
+            const box = document.createElement("div");
+            box.className = "square default";
+            box.style.width = boxSize + "px";
+            box.style.height = boxSize + "px";
+            currentRow.appendChild(box);
+            box.addEventListener("mouseover",() => {
+                box.classList.remove("default");
+                box.classList.add("box-hover");
+            });
+            box.addEventListener("mouseout",() => {
+                box.classList.remove("box-hover");
+                box.classList.add("default");
+            });
+        }
+        container.appendChild(currentRow);
+    }
+    main.appendChild(container);
 }
+const GRID_WIDTH = 600;
+const GRID_HEIGHT = 600;
+const resizeButton = document.querySelector(".button");
+let container = document.querySelector(".container");
+const main = document.querySelector("body");
+createGird(16);
+resizeButton.addEventListener("click", () => {
+    const squaresPerSide = prompt("Number of squares per side: ");
+    createGird(squaresPerSide);
+});
