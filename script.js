e8e5616c61703a84198c9b351973d0f3f42a3398
dot-sky <108539845+dot-sky@users.noreply.github.com>
2024-01-20 07:00:29 -0400

    Add hover effect

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const container = document.querySelector(".container");
 for (let i = 0; i < 16*16; i++) {
     const element = document.createElement("div");
-    element.className = "square";
+    element.className = "square default";
     container.appendChild(element);
+    element.addEventListener("mouseover",() => {
+        element.classList.remove("default");
+        element.classList.add("box-hover");
+    });
+    element.addEventListener("mouseout",() => {
+        element.classList.remove("box-hover");
+        element.classList.add("default");
+    });
 }
